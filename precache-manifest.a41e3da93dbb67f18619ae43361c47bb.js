e2be97c692b2263d7427a86c7aa796f23f33a88a
mandolyte <cecil.new@gmail.com>
2021-07-17 07:58:42 -0400

    Updates

---
+++ precache-manifest.a41e3da93dbb67f18619ae43361c47bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f252e964600afb4be7f",
+    "url": "build/2.7f252e96.js"
+  },
+  {
+    "revision": "0c82845084b453bda1e1adf4ee1de14d",
+    "url": "build/2.7f252e96.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c28f4d3c213216eb537",
+    "url": "build/bundle.7c28f4d3.js"
+  },
+  {
+    "revision": "f3693a1ff451ee4e4c62",
+    "url": "build/main.f3693a1f.js"
+  },
+  {
+    "revision": "1d8a11324c3c9e3a05cda54ccd93f1dc",
+    "url": "index.html"
+  }
+]);
